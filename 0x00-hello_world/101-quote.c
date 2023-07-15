72ce785c3549e028580d53c73bc5d5a9617448e0
Barbara-paula <paulabarbara161@gmail.com>
2023-07-15 06:27:00 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - A program that prints a line
+ * return 1
+ */
+int main(void)
+{
+	fput("and that piece of art is very useful\" - DoraKorpar, 2015-10-19\n", stdout);
+return (1);
