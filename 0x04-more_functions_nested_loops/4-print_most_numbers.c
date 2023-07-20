50ccebe516fb9886157be063d413831dae61c349
Barbara-paula <paulabarbara161@gmail.com>
2023-07-20 13:14:04 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * print_most_numbers 01356789
+ * print_most_numbers - prints 01356789
  * Return: void
  */
 
 void print_most_numbers(void)
-
 {
 char c;
 	for (c = '0'; c <= '9'; c++)
