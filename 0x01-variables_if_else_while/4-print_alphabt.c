8f975876a3f345712e58b1f315aa4c447f4c7124
Barbara-paula <paulabarbara161@gmail.com>
2023-08-03 14:41:08 -0700

    0-puts_recursion.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * prints in lowercase followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		if (a == 101 || a == 113)
+		{
+			a++;
+			continue;
+		}
+		putchar(a);
+			n++;
+	}
+	putchar('\n');
+	return (0);
+}
+	
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar(45);
+		_putchar('-');
 		i = -1;
 	}
 	if (i / 10)
