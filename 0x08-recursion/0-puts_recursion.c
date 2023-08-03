f0f5168cca3f91de8f6eb7c20a3f627ab5471c7c
Barbara-paula <paulabarbara161@gmail.com>
2023-08-03 15:11:57 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-        if (*s)
-        {
-                _putchar(*s);
-                        _puts_recursions(s + 1);
-        }
-        else
-                _putchar('\n');
+	if (*s)
+	{
+		_putchar(*s)
+			_puts_recursions(s + 1);
+	}
+	else
+		_putchar('\n');
 }
